f8e45b48eb9f9ee3df15b8746101b037f672d73a
elvvvv <113820719+elvatimang15@users.noreply.github.com>
2023-06-18 20:50:48 -0700

    Menambahkan Tugas Final (Aplikasi Kasir)

---
+++ App/src/Koneksi.java
@@ -0,0 +1,33 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+/**
+ *
+ * @author lenovo
+ */
+public class Koneksi {
+    private static Connection koneksi;
+
+    public static Connection getKoneksi() {
+    if (koneksi == null) {
+        try {
+            String url = "jdbc:mysql://localhost:3306/db_toko";
+            String user = "root";
+            String password = "";
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            koneksi = DriverManager.getConnection(url, user, password);
+            System.out.println("Berhasil");
+        } catch (Exception e) {
+            System.out.println("Error");
+        }
+    }
+    return koneksi;
+}
+    public static void main(String args[]){
+        getKoneksi();
+    }
+}
